0269d97ae2e7077a3e6208837e980c997f957def
Gurpreet Singh Bhachu <gurpreetbhachu@github.com>
2020-07-22 19:39:46 -0400

    Updates

---
+++ precache-manifest.0ff45f0b42cbb3bd74eaa381c18bdb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aacd0ed8d4d6d9f630e7174887ea776",
+    "url": "/gurpreetsbhachu/github-job-api/index.html"
+  },
+  {
+    "revision": "57d7b1bd0fb424cfa457",
+    "url": "/gurpreetsbhachu/github-job-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ab5abeab672eb45f11c",
+    "url": "/gurpreetsbhachu/github-job-api/static/js/2.6f000422.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/gurpreetsbhachu/github-job-api/static/js/2.6f000422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d7b1bd0fb424cfa457",
+    "url": "/gurpreetsbhachu/github-job-api/static/js/main.095f3f4e.chunk.js"
+  },
+  {
+    "revision": "088a2700f5c2a322ea40",
+    "url": "/gurpreetsbhachu/github-job-api/static/js/runtime-main.d77dbb7f.js"
+  }
+]);
